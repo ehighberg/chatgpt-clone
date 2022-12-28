65cebf5c15ccb4ced7f41561c7d245798973b2b0
ehighberg <errol.highberg@gmail.com>
2022-12-27 23:09:50 -0500

    first api call

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import { config as envconfig } from 'dotenv'
 import { Configuration, OpenAIApi } from "openai"
 
+envconfig()
+
 const configuration = new Configuration({
     organization: "org-brmCUWqjyEqs9ItpcPDiCBKn",
     apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration)
-// const response = await openai.listEngines()
+
+async function callApi() {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    })
+    console.log(response.data.choices[0].text)
+}
+
+callApi()
