ca0481b132050cf2fea40d7dcc079de6db499e29
ehighberg <errol.highberg@gmail.com>
2022-12-27 20:20:42 -0500

    add chat input

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,9 @@ function App() {
         </div>
       </aside>
       <section className="chatbox">
-
+        <div className="chat-input-holder">
+          <textarea rows="1" className="chat-input-textarea" placeholder="Type your prompt here"></textarea>
+        </div>
       </section>
     </div>
   );
