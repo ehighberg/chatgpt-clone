a86ff82d4ce8fc6cb6e2977144e0c10e4b69b64f
ehighberg <errol.highberg@gmail.com>
2022-12-27 22:56:44 -0500

    initial openai import

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const { config } = require('dotenv');
+import { config as envconfig } from 'dotenv'
+import { Configuration, OpenAIApi } from "openai"
+
+const configuration = new Configuration({
+    organization: "org-brmCUWqjyEqs9ItpcPDiCBKn",
+    apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+// const response = await openai.listEngines()
