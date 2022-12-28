83859fc048e40cd127995d45ae85610e3bc8ff2d
ehighberg <errol.highberg@gmail.com>
2022-12-27 19:32:32 -0500

    style new chat button

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-        Aside
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
       <section className="chatbox">
 
